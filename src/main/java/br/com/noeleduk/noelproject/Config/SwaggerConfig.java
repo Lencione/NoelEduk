c3210fd7e9944d09de409153c6b3c41c72609cca
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-09-12 20:26:42 -0300

    feat: configure SwaggerConfig.java properly

--- src/main/java/br/com/noeleduk/noelproject/Config/SwaggerConfig.java
+++ src/main/java/br/com/noeleduk/noelproject/Config/SwaggerConfig.java
@@ -19,16 +19,19 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.controller.api"))
+                .apis(RequestHandlerSelectors.basePackage("br.com.noeleduk.noelproject.Controllers"))
                 .paths(PathSelectors.any())
                 .build().apiInfo(apiInfoMetaData());
     }
 
     private ApiInfo apiInfoMetaData() {
 
-        return new ApiInfoBuilder().title("API Documentation")
-                .description("Describing the purpose of the API")
-                .contact(new Contact("Dev-Team", "https://www.payment.example.com/", "nzuwera2002@gmail.com"))
+        return new ApiInfoBuilder().title("NoelEduk API Docs")
+                .description("O propósito desta documentação é evidenciar todas as funcionalidades do servidor.")
+                .contact(new Contact("Wesley Lencione", "https://github.com/lencione", "wesley@lencione.com.br"))
+                .contact(new Contact("Rafael Menegon", "https://github.com/devrafamenegon", "dev.rafaelmenegon@gmail.com"))
+                .contact(new Contact("Enzo Sabino", "https://github.com/onnibas", "enzo.sabino199@al.unieduk.com.br"))
+                .contact(new Contact("José Pagotto", "https://github.com/heitorpagotto", "jose.pagotto006@al.unieduk.com.br"))
                 .license("Apache 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
                 .version("1.0.0")
