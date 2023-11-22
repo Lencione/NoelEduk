fb16d988c92164fb3d7ee83a6ada5d650ae8ef6b
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-11-22 00:02:23 -0300

    test: add auth controller tests

--- src/test/java/br/com/noeleduk/noelproject/commons/UserConstraints.java
+++ src/test/java/br/com/noeleduk/noelproject/commons/UserConstraints.java
@@ -1,2 +1,51 @@
-package br.com.noeleduk.noelproject.commons;public class UserConstraints {
+package br.com.noeleduk.noelproject.commons;
+
+import br.com.noeleduk.noelproject.dto.response.ResponseDto;
+import br.com.noeleduk.noelproject.dto.user.CreateUserDto;
+import br.com.noeleduk.noelproject.dto.user.LoggedUserDto;
+import br.com.noeleduk.noelproject.dto.user.LoginRequestDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+public class UserConstraints {
+    public static final String USER_FOUND = "User found";
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String INVALID_PASSWORD = "Invalid password";
+    public static final String INVALID_EMAIL = "Email invalido (prof.unieduk.com.br ou al.unieduk.com.br)";
+    public static final String USER_CREATED = "User created with success!";
+    public static final String DUPLICATED_EMAIL = "O E-mail ja esta em uso!";
+    public static final String DUPLICATED_RA = "O RA ja esta em uso";
+    public static final String DUPLICATED_CPF = "O CPF ja esta em uso";
+    public static final String DUPLICATED_RG = "O RG ja esta em uso";
+    public static final String STUDENT_EMAIL = "al.unieduk.com.br";
+    public static final String TEACHER_EMAIL = "prof.unieduk.com.br";
+    public static final String STUDENT_ROLE = "student";
+    public static final String TEACHER_ROLE = "teacher";
+
+    public static final LoginRequestDto LOGIN_REQUEST_DTO = new LoginRequestDto("email@al.unieduk.com.br", "12345678");
+    public static final LoginRequestDto INVALID_LOGIN_REQUEST_DTO = new LoginRequestDto("email@al.unieduk.com.br", "12345678");
+    public static final LoginRequestDto NOT_FOUND_LOGIN_REQUEST_DTO = new LoginRequestDto("not_email@al.unieduk.com.br", "87654321");
+
+    public static final LoggedUserDto LOGGED_USER_DTO = new LoggedUserDto("a45f2350-88ce-11ee-b9d1-0242ac120002", LocalDateTime.now(), "student");
+    public static final ResponseEntity<ResponseDto> LOGIN_USER_RESPONSE = ResponseEntity.ok().body(
+        new ResponseDto(USER_FOUND, true, LOGGED_USER_DTO)
+    );
+    public static final ResponseEntity<ResponseDto> NOT_FOUND_LOGIN_USER_RESPONSE = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+        new ResponseDto(USER_NOT_FOUND, false, null)
+    );
+    public static final ResponseEntity<ResponseDto> INVALID_LOGIN_USER_RESPONSE = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+        new ResponseDto(INVALID_PASSWORD, false, null)
+    );
+
+    public static final CreateUserDto CREATE_USER_DTO = new CreateUserDto("pix.silva@al.unieduk.com.br", "12345678", "53923153815", "641075586", "Pix da Silva", "19996328763", "12224577","https://gravatar.com/avatar/dd95411d45214301e2673e1c6a8f5d47?s=400&d=robohash&r=x");
+
+    public static final ResponseEntity<ResponseDto> CREATE_USER_RESPONSE = ResponseEntity.ok().body(
+            new ResponseDto(USER_CREATED, true, LOGGED_USER_DTO)
+    );
+
+    public static final ResponseEntity<ResponseDto> DUPLICATED_EMAIL_CREATE_USER_RESPONSE = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            new ResponseDto(DUPLICATED_EMAIL, false, null)
+    );
 }
--- src/test/java/br/com/noeleduk/noelproject/controllers/auth/AuthControllerTest.java
+++ src/test/java/br/com/noeleduk/noelproject/controllers/auth/AuthControllerTest.java
@@ -1,22 +1,15 @@
 package br.com.noeleduk.noelproject.controllers.auth;
 
 import br.com.noeleduk.noelproject.dto.response.ResponseDto;
-import br.com.noeleduk.noelproject.dto.user.CreateUserDto;
-import br.com.noeleduk.noelproject.dto.user.GetUserDto;
-import br.com.noeleduk.noelproject.dto.user.LoggedUserDto;
-import br.com.noeleduk.noelproject.dto.user.LoginRequestDto;
 import br.com.noeleduk.noelproject.services.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import static br.com.noeleduk.noelproject.commons.UserConstraints.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,77 +21,42 @@ public class AuthControllerTest {
     private UserService userService;
 
     @Test
-    public void testLoginSuccess() {
-        LoginRequestDto loginRequest = new LoginRequestDto();
-        LoggedUserDto loggedUserDto = new LoggedUserDto();
+    public void loginUser_withValidEmailAndPassword_returnsLoggedUser() {
+        when(userService.login(LOGIN_REQUEST_DTO)).thenReturn(LOGGED_USER_DTO);
 
-        when(userService.login(loginRequest)).thenReturn(loggedUserDto);
+        ResponseEntity<ResponseDto> sut = authController.login(LOGIN_REQUEST_DTO);
 
-        ResponseEntity<ResponseDto> response = authController.login(loginRequest);
-        verify(userService, times(1)).login(loginRequest);
-
-        assert response.getStatusCode() == HttpStatus.OK;
-        assert response.getBody() != null;
-        assert response.getBody().getMessage().equals("User found");
-        assert response.getBody().getData().equals(loggedUserDto);
+        verify(userService, times(1)).login(LOGIN_REQUEST_DTO);
+        assertThat(sut).isEqualTo(LOGIN_USER_RESPONSE);
     }
 
     @Test
-    public void testLoginFailure() {
-        LoginRequestDto loginRequest = new LoginRequestDto(); // preencha com os detalhes de login apropriados
+    public void loginUser_withInvalidEmailAndPassword_returnsInvalidPassword() {
+        when(userService.login(LOGIN_REQUEST_DTO)).thenThrow(new RuntimeException(INVALID_PASSWORD));
 
-        when(userService.login(loginRequest)).thenThrow(new RuntimeException("Login failed"));
+        ResponseEntity<ResponseDto> sut = authController.login(LOGIN_REQUEST_DTO);
 
-        ResponseEntity<ResponseDto> response = authController.login(loginRequest);
-
-        // Verifique se o serviço de usuário foi chamado corretamente
-        verify(userService, times(1)).login(loginRequest);
-
-        // Verifique se a resposta HTTP está correta
-        assert response.getStatusCode() == HttpStatus.BAD_REQUEST;
-
-        // Verifique o conteúdo da resposta
-        assert response.getBody() != null;
-//        assert !response.getBody().isSuccess();
-        assert response.getBody().getMessage().equals("Login failed");
+        verify(userService, times(1)).login(LOGIN_REQUEST_DTO);
+        assertThat(sut).isEqualTo(INVALID_LOGIN_USER_RESPONSE);
     }
 
     @Test
-    public void testCreateUserSuccess() {
-        CreateUserDto createUserDto = new CreateUserDto();
-        LoggedUserDto loggedUserDto = new LoggedUserDto();
+    public void createUser_withValidEmailAndPassword_returnsCreatedUser() {
+        when(userService.create(CREATE_USER_DTO)).thenReturn(LOGGED_USER_DTO);
 
-        when(userService.create(createUserDto)).thenReturn(loggedUserDto);
+        ResponseEntity<ResponseDto> sut = authController.createUser(CREATE_USER_DTO);
 
-        ResponseEntity<ResponseDto> response = authController.createUser(createUserDto);
-
-        // Verifique se o serviço de usuário foi chamado corretamente
-        verify(userService, times(1)).create(createUserDto);
-
-        // Verifique se a resposta HTTP está correta
-        assert response.getStatusCode() == HttpStatus.OK;
-
-        // Verifique o conteúdo da resposta
-        assert response.getBody() != null;
-        assert response.getBody().getMessage().equals("User created with success!");
+        verify(userService, times(1)).create(CREATE_USER_DTO);
+        assertThat(sut).isEqualTo(CREATE_USER_RESPONSE);
     }
 
     @Test
-    public void testCreateUserFailure() {
-        CreateUserDto createUserDto = new CreateUserDto(); // preencha com os detalhes do usuário apropriados
+    public void createUser_withDuplicatedEmail_returnsDuplicatedEmail() {
+        when(userService.create(CREATE_USER_DTO)).thenThrow(new RuntimeException(DUPLICATED_EMAIL));
 
-        when(userService.create(createUserDto)).thenThrow(new RuntimeException("User creation failed"));
+        ResponseEntity<ResponseDto> sut = authController.createUser(CREATE_USER_DTO);
 
-        ResponseEntity<ResponseDto> response = authController.createUser(createUserDto);
-
-        // Verifique se o serviço de usuário foi chamado corretamente
-        verify(userService, times(1)).create(createUserDto);
-
-        // Verifique se a resposta HTTP está correta
-        assert response.getStatusCode() == HttpStatus.BAD_REQUEST;
-
-        // Verifique o conteúdo da resposta
-        assert response.getBody() != null;
-        assert response.getBody().getMessage().equals("User creation failed");
+        verify(userService, times(1)).create(CREATE_USER_DTO);
+        assertThat(sut).isEqualTo(DUPLICATED_EMAIL_CREATE_USER_RESPONSE);
     }
 }
