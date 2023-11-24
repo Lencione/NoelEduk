3e6f8456974118a2682d5200933f66fb0107efe6
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-11-24 20:09:39 -0300

    test: add more teacher controller tests

--- src/test/java/br/com/noeleduk/noelproject/commons/ClassConstraints.java
+++ src/test/java/br/com/noeleduk/noelproject/commons/ClassConstraints.java
@@ -1,8 +1,10 @@
 package br.com.noeleduk.noelproject.commons;
 
+import br.com.noeleduk.noelproject.dto.classes.AddStudentToClassDto;
 import br.com.noeleduk.noelproject.dto.classes.CreateClassDto;
 import br.com.noeleduk.noelproject.dto.classes.GetClassDto;
 import br.com.noeleduk.noelproject.dto.response.ResponseDto;
+import br.com.noeleduk.noelproject.dto.user.GetUserDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -12,10 +14,12 @@ import java.util.UUID;
 
 public class ClassConstraints {
 
-    public static final String MOCK_UUID = "1376e95c-89a2-11ee-b9d1-0242ac120002";
+    public static final UUID MOCK_UUID = UUID.fromString("1376e95c-89a2-11ee-b9d1-0242ac120002");
     public static final String CLASS_CREATED = "Created class successfully";
     public static final String CLASSES_FOUND = "Classes found successfully";
     public static final String USER_ADDED_TO_CLASS = "User added to class successfully";
+    public static final String STUDENT_ADDED_TO_CLASS = "Student added to class with success";
+
     public static final String STUDENTS_FOUND = "Students found successfully";
     public static final String TEACHERS_FOUND = "Teachers found";
     public static final String SUBJECTS_FOUND = "Subjects found successfully";
@@ -26,7 +30,7 @@ public class ClassConstraints {
     public static final String CLASS_ADDED_TO_SUBJECT = "Class added to subject successfully";
     public static final String INVALID_TEACHER_DOCUMENT = "Invalid teacher document";
     public static final CreateClassDto CREATE_CLASS_DTO = new CreateClassDto("Banco de Dados 2", 4);
-    public static final String TEACHER_DOCUMENT = "12224579";
+    public static final String MOCK_DOCUMENT = "12224579";
     public static final ResponseEntity<ResponseDto> CREATE_CLASS_RESPONSE = ResponseEntity.ok().body(
         new ResponseDto(CLASS_CREATED, true, CLASS_CREATED)
     );
@@ -34,22 +38,49 @@ public class ClassConstraints {
         new ResponseDto(INVALID_TEACHER_DOCUMENT, false, null)
     );
 
-    public static final GetClassDto GET_CLASS_DTO_1 = new GetClassDto(UUID.fromString(MOCK_UUID), "Banco de Dados 2", 4);
+    public static final GetClassDto GET_CLASS_DTO_1 = new GetClassDto(MOCK_UUID, "Banco de Dados 2", 4);
 
-    public static final GetClassDto GET_CLASS_DTO_2 = new GetClassDto(UUID.fromString(MOCK_UUID), "Estrutura de Dados", 6);
+    public static final GetClassDto GET_CLASS_DTO_2 = new GetClassDto(MOCK_UUID, "Estrutura de Dados", 6);
 
     public static final List<GetClassDto> GET_CLASS_DTO_LIST = Arrays.asList(
         GET_CLASS_DTO_1,
         GET_CLASS_DTO_2
     );
 
+    public static final GetUserDto GET_STUDENT_DTO_1 = new GetUserDto("José Mozart", "jose.mozart789@al.unieduk.com.br", "1224875", "564", "https://gravatar.com/avatar/dd95411d45214301e2673e1c6a8f5d47?s=400&d=robohash&r=x", "112");
+    public static final GetUserDto GET_STUDENT_DTO_2 = new GetUserDto("Luis Henrique", "luis.henrique789@al.unieduk.com.br", "1224175", "1034", "https://gravatar.com/avatar/dd95411d45214301e2673e1c6a8f5d47?s=400&d=robohash&r=x", "322");
+
+    public static final List<GetUserDto> GET_STUDENT_DTO_LIST = Arrays.asList(
+            GET_STUDENT_DTO_1,
+            GET_STUDENT_DTO_2
+    );
+
     public static final ResponseEntity<ResponseDto> GET_CLASSES_RESPONSE = ResponseEntity.ok().body(
-            new ResponseDto(CLASSES_FOUND, true, GET_CLASS_DTO_LIST)
+        new ResponseDto(CLASSES_FOUND, true, GET_CLASS_DTO_LIST)
+    );
+
+    public static final ResponseEntity<ResponseDto> GET_STUDENTS_BY_CLASS_ID_RESPONSE = ResponseEntity.ok().body(
+            new ResponseDto(STUDENTS_FOUND, true,GET_STUDENT_DTO_LIST)
     );
 
     public static final ResponseEntity<ResponseDto> INVALID_GET_CLASSES_RESPONSE = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+        new ResponseDto(INVALID_TEACHER_DOCUMENT, false, null)
+    );
+
+    public static final ResponseEntity<ResponseDto> INVALID_GET_STUDENTS_RESPONSE = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
             new ResponseDto(INVALID_TEACHER_DOCUMENT, false, null)
     );
 
-    
+    public static final ResponseEntity<ResponseDto> INVALID_GET_CLASS_RESPONSE = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            new ResponseDto(INVALID_TEACHER_DOCUMENT, false, null)
+    );
+
+    public static final AddStudentToClassDto ADD_STUDENT_TO_CLASS_DTO = new AddStudentToClassDto(MOCK_DOCUMENT);
+
+    public static final ResponseEntity<ResponseDto> ADD_STUDENT_TO_CLASS_RESPONSE = ResponseEntity.ok().body(
+            new ResponseDto(USER_ADDED_TO_CLASS, true, STUDENT_ADDED_TO_CLASS)
+    );
+    public static final ResponseEntity<ResponseDto> INVALID_ADD_STUDENT_TO_CLASS_RESPONSE = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+        new ResponseDto(INVALID_TEACHER_DOCUMENT, false, null)
+    );
 }
--- src/test/java/br/com/noeleduk/noelproject/controllers/teacher/TeacherControllerTest.java
+++ src/test/java/br/com/noeleduk/noelproject/controllers/teacher/TeacherControllerTest.java
@@ -22,42 +22,82 @@ public class TeacherControllerTest {
     private TeacherService teacherService;
 
     @Test
-    public void createClass_withValidTeacherDocument_returnsCreatedClass() {
-        when(teacherService.createClass(TEACHER_DOCUMENT, CREATE_CLASS_DTO)).thenReturn(CLASS_CREATED);
+    public void createClass_withValidData_returnsCreatedClass() {
+        when(teacherService.createClass(MOCK_DOCUMENT, CREATE_CLASS_DTO)).thenReturn(CLASS_CREATED);
 
-        ResponseEntity<ResponseDto> sut = teacherController.createClass(TEACHER_DOCUMENT, CREATE_CLASS_DTO);
+        ResponseEntity<ResponseDto> sut = teacherController.createClass(MOCK_DOCUMENT, CREATE_CLASS_DTO);
 
-        verify(teacherService, times(1)).createClass(TEACHER_DOCUMENT, CREATE_CLASS_DTO);
+        verify(teacherService, times(1)).createClass(MOCK_DOCUMENT, CREATE_CLASS_DTO);
         assertThat(sut).isEqualTo(CREATE_CLASS_RESPONSE);
     }
 
     @Test
     void createClass_withInvalidTeacherDocument_returnsInvalidTeacher() {
-        when(teacherService.createClass(TEACHER_DOCUMENT, CREATE_CLASS_DTO)).thenThrow(new RuntimeException(INVALID_TEACHER_DOCUMENT));
+        when(teacherService.createClass(MOCK_DOCUMENT, CREATE_CLASS_DTO)).thenThrow(new RuntimeException(INVALID_TEACHER_DOCUMENT));
 
-        ResponseEntity<ResponseDto> sut = teacherController.createClass(TEACHER_DOCUMENT, CREATE_CLASS_DTO);
+        ResponseEntity<ResponseDto> sut = teacherController.createClass(MOCK_DOCUMENT, CREATE_CLASS_DTO);
 
-        verify(teacherService, times(1)).createClass(TEACHER_DOCUMENT, CREATE_CLASS_DTO);
+        verify(teacherService, times(1)).createClass(MOCK_DOCUMENT, CREATE_CLASS_DTO);
         assertThat(sut).isEqualTo(INVALID_CREATE_CLASS_RESPONSE);
     }
 
     @Test
     public void getClasses_withValidTeacherDocument_returnsClasses() {
-        when(teacherService.getClassesByTeacherDocument(TEACHER_DOCUMENT)).thenReturn(GET_CLASS_DTO_LIST);
+        when(teacherService.getClassesByTeacherDocument(MOCK_DOCUMENT)).thenReturn(GET_CLASS_DTO_LIST);
 
-        ResponseEntity<ResponseDto> sut = teacherController.getClasses(TEACHER_DOCUMENT);
+        ResponseEntity<ResponseDto> sut = teacherController.getClasses(MOCK_DOCUMENT);
 
-        verify(teacherService, times(1)).getClassesByTeacherDocument(TEACHER_DOCUMENT);
+        verify(teacherService, times(1)).getClassesByTeacherDocument(MOCK_DOCUMENT);
         assertThat(sut).isEqualTo(GET_CLASSES_RESPONSE);
     }
 
     @Test
     void getClasses_withInvalidTeacherDocument_returnsInvalidTeacher() {
-        when(teacherService.getClassesByTeacherDocument(TEACHER_DOCUMENT)).thenThrow(new RuntimeException(INVALID_TEACHER_DOCUMENT));
+        when(teacherService.getClassesByTeacherDocument(MOCK_DOCUMENT)).thenThrow(new RuntimeException(INVALID_TEACHER_DOCUMENT));
 
-        ResponseEntity<ResponseDto> sut = teacherController.getClasses(TEACHER_DOCUMENT);
+        ResponseEntity<ResponseDto> sut = teacherController.getClasses(MOCK_DOCUMENT);
 
-        verify(teacherService, times(1)).getClassesByTeacherDocument(TEACHER_DOCUMENT);
+        verify(teacherService, times(1)).getClassesByTeacherDocument(MOCK_DOCUMENT);
         assertThat(sut).isEqualTo(INVALID_GET_CLASSES_RESPONSE);
+    }
+
+    @Test
+    public void addStudentToClass_withValidData_returnsAddedUser() {
+        when(teacherService.addStudentToClass(MOCK_DOCUMENT, MOCK_UUID, ADD_STUDENT_TO_CLASS_DTO)).thenReturn(STUDENT_ADDED_TO_CLASS);
+
+        ResponseEntity<ResponseDto> sut = teacherController.addStudentToClass(MOCK_DOCUMENT, MOCK_UUID, ADD_STUDENT_TO_CLASS_DTO);
+
+        verify(teacherService, times(1)).addStudentToClass(MOCK_DOCUMENT, MOCK_UUID, ADD_STUDENT_TO_CLASS_DTO);
+        assertThat(sut).isEqualTo(ADD_STUDENT_TO_CLASS_RESPONSE);
+    }
+
+    @Test
+    void addStudentToClass_withInvalidTeacherDocument_returnsInvalidTeacher() {
+        when(teacherService.addStudentToClass(MOCK_DOCUMENT, MOCK_UUID, ADD_STUDENT_TO_CLASS_DTO)).thenThrow(new RuntimeException(INVALID_TEACHER_DOCUMENT));
+
+        ResponseEntity<ResponseDto> sut = teacherController.addStudentToClass(MOCK_DOCUMENT, MOCK_UUID, ADD_STUDENT_TO_CLASS_DTO);
+
+        verify(teacherService, times(1)).addStudentToClass(MOCK_DOCUMENT, MOCK_UUID, ADD_STUDENT_TO_CLASS_DTO);
+        assertThat(sut).isEqualTo(INVALID_ADD_STUDENT_TO_CLASS_RESPONSE);
+    }
+
+    @Test
+    public void getStudentsByClassId_withValidData_returnsClass() {
+        when(teacherService.getStudentsByClassId(MOCK_DOCUMENT, MOCK_UUID)).thenReturn(GET_STUDENT_DTO_LIST);
+
+        ResponseEntity<ResponseDto> sut = teacherController.getStudentsByClassId(MOCK_UUID, MOCK_DOCUMENT);
+
+        verify(teacherService, times(1)).getStudentsByClassId(MOCK_DOCUMENT, MOCK_UUID);
+        assertThat(sut).isEqualTo(GET_STUDENTS_BY_CLASS_ID_RESPONSE);
+    }
+
+    @Test
+    void getStudentsByClassId_withInvalidTeacherDocument_returnsInvalidTeacher() {
+        when(teacherService.getStudentsByClassId(MOCK_DOCUMENT, MOCK_UUID)).thenThrow(new RuntimeException(INVALID_TEACHER_DOCUMENT));
+
+        ResponseEntity<ResponseDto> sut = teacherController.getStudentsByClassId(MOCK_UUID, MOCK_DOCUMENT);
+
+        verify(teacherService, times(1)).getStudentsByClassId(MOCK_DOCUMENT, MOCK_UUID);
+        assertThat(sut).isEqualTo(INVALID_GET_STUDENTS_RESPONSE);
     }
 }
