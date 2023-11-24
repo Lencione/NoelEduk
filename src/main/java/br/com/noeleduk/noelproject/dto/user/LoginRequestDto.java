76ff0e7e885baaddf98c17bc988ffe89daa7063d
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-11-24 20:20:48 -0300

    feat: add all args constructor to create mocks

--- src/main/java/br/com/noeleduk/noelproject/dto/user/LoginRequestDto.java
+++ src/main/java/br/com/noeleduk/noelproject/dto/user/LoginRequestDto.java
@@ -1,8 +1,10 @@
 package br.com.noeleduk.noelproject.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginRequestDto {
   private String email;
   private String password;
