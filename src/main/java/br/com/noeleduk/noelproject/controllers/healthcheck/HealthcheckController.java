120cf802282be5d63cfb30971193c9e6f671014a
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-11-30 15:07:02 -0300

    feat: rename TestController to HealthcheckController

--- src/main/java/br/com/noeleduk/noelproject/controllers/healthcheck/HealthcheckController.java
+++ src/main/java/br/com/noeleduk/noelproject/controllers/healthcheck/HealthcheckController.java
@@ -1,14 +1,14 @@
-package br.com.noeleduk.noelproject.controllers;
+package br.com.noeleduk.noelproject.controllers.healthcheck;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
-public class Test {
+@RequestMapping("/healthcheck")
+public class HealthcheckController {
   @GetMapping("/")
-  public String test(){
-    return "test";
+  public String healthcheck(){
+    return "OK";
   }
 }
